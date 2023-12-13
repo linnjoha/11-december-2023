a52eda4d384ece6355a6f79a9859244cc3c64589
linnjoha <linn.wallin@hotmail.com>
2023-12-13 07:44:40 +0100

    game

---
+++ memory-game-exercise/dist/js/index.js
@@ -0,0 +1,71 @@
+//blanda kort
+//jämföra kort
+//- if på klick1 och klick2 !är data-card===data-card
+//else gå vidare på nästa
+// if alla cardel === flip = win. overlay =display
+//X= location reload
+const cardEl = document.querySelectorAll(".memory-card");
+const cardElArray = Array.from(cardEl);
+let [click1, click2] = [null, null];
+//mixa korten
+const mixCards = () => {
+    for (let i = cardElArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = cardElArray[i];
+        cardElArray[i] = cardElArray[j];
+        cardElArray[j] = temp;
+    }
+    const memoryCards = document.querySelector(".memory-cards");
+    for (let i = 0; i < cardElArray.length; i++) {
+        memoryCards.appendChild(cardElArray[i]);
+    }
+};
+mixCards();
+cardEl.forEach((card) => {
+    card.addEventListener("click", () => {
+        compareCards(card);
+    });
+});
+const compareCards = (card) => {
+    console.log(card);
+    // currentData. datan på korten som ska jämföras
+    let currentData = card.getAttribute("data-card");
+    //booleans-variabler för jämförelse
+    const noClickIsSet = !click1 && !click2;
+    const hasClick1ButNot2 = click1 && !click2;
+    const cardIsSame = card === (click1 === null || click1 === void 0 ? void 0 : click1.card);
+    //sätter click1
+    if (noClickIsSet) {
+        card.classList.add("flip");
+        click1 = { data: currentData, card: card };
+    }
+    //sätter click2 ifall card inte är samma för att undvika dubbelklick
+    else if (hasClick1ButNot2 && !cardIsSame) {
+        card.classList.add("flip");
+        click2 = { data: currentData, card: card };
+    }
+    const hasBothClickedCards = click1 && click2;
+    if (hasBothClickedCards) {
+        const isNoMatch = click1.data !== click2.data;
+        if (isNoMatch) {
+            [click1.card, click2.card].forEach((f) => {
+                setTimeout(() => {
+                    f.classList.remove("flip");
+                }, 2000);
+            });
+        }
+        //nollställer variablerna för att kunna använda dom och jämföra igen.
+        [click1, click2] = [null, null];
+    }
+    const allCardHasFlip = cardElArray.every((card) => card.classList.contains("flip"));
+    if (allCardHasFlip) {
+        winTheGame();
+    }
+};
+//add .show overlay om alla korten has .flip
+const winTheGame = () => {
+    const overlay = document.querySelector(".overlay");
+    overlay.classList.add("show");
+    const exitEl = document.querySelector(".close");
+    exitEl.addEventListener("click", () => location.reload());
+};
